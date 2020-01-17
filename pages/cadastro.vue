5b4d68aa0c4b983a98c5543e1df5b900daf282f5
Juan1329 <silva.juan.almeida@gmail.com>
2020-01-17 20:59:59 -0300

    initial repository

---
+++ pages/cadastro.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="email" v-model="email" placeholder="email">
+        <input type="text" v-model="nome" placeholder="nome">
+        <input type="password" v-model="senha" placeholder="senha">
+        <input type="submit" v-on:click="pronto" value="pronto">
+        <input type="submit" v-on:click="voltar" value="voltar">
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            email: '',
+            nome: '',
+            senha: '',
+            infos: ''
+        }
+    },
+    methods: {
+        pronto(){
+            this.infos = 'Cadastrado: '+ this.email + this.nome + this.senha; 
+        },
+        voltar(){
+            this.$router.push()
+        }
+    }
+}
+</script>
